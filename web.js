ea2439df77a7ac0f87db2d29d467252336c7eb37
Xuegen Jin <xuegen.jin@gmail.com>
2013-07-06 20:10:17 +0000

    display index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var content  = fs.readFileSync("index.html");
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
